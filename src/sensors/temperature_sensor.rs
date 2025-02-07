bb25cbf3ef4f5080d48f8bd1c1eae6a2af39312c
gwj <sfwwslm@gmail.com>
2025-02-07 13:40:38 +0800

    `TemperatureSensor`新增字段

--- src/sensors/temperature_sensor.rs
+++ src/sensors/temperature_sensor.rs
@@ -9,9 +9,13 @@ use crate::Temperature;
 pub struct TemperatureSensor {
 	pub(crate) unit: String,
 	pub(crate) label: Option<String>,
+	/// 当前温度
 	pub(crate) current: Temperature,
 	pub(crate) max: Option<Temperature>,
 	pub(crate) crit: Option<Temperature>,
+	pub(crate) min: Option<Temperature>,
+	/// 从`/sys/class/hwmon/hwmon0`中提取`hwmon0`作为传感器的id，用于标识传感器
+	pub(crate) sensor_id: Option<String>,
 }
 
 impl TemperatureSensor {
@@ -35,8 +39,17 @@ impl TemperatureSensor {
 		self.max.as_ref()
 	}
 
+	/// 在`nvme`硬盘上的最低温度
+	pub fn min(&self) -> Option<&Temperature> {
+		self.min.as_ref()
+	}
+
 	/// Returns critical trip point for sensor if available.
 	pub fn critical(&self) -> Option<&Temperature> {
 		self.crit.as_ref()
+	}
+
+	pub fn sensor_id(&self)->Option<&str>{
+		self.sensor_id.as_deref()
 	}
 }
