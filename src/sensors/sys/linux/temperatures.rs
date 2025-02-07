f150b6900d74b3b01ab1b15affa7a692e7a71656
gwj <sfwwslm@gmail.com>
2025-02-07 13:41:43 +0800

    `TemperatureSensor`新增字段

--- src/sensors/sys/linux/temperatures.rs
+++ src/sensors/sys/linux/temperatures.rs
@@ -8,15 +8,29 @@ use std::path::PathBuf;
 use crate::sensors::TemperatureSensor;
 use crate::{glob, read_file, Error, Result, Temperature};
 
+/// 创建一个新的 `OsString`，其容量足够容纳 `prefix` 和 `postfix` 字符串的组合长度。
+/// 这有助于在将这些字符串附加到 `name` 时避免重新分配。
 #[inline]
 fn file_name(prefix: &OsStr, postfix: &[u8]) -> OsString {
 	let mut name = OsString::with_capacity(prefix.len() + postfix.len());
 	name.push(prefix);
 	name.push(OsStr::from_bytes(postfix));
-
 	name
 }
 
+/// 读取给定路径的文件内容，并将内容作为字符串返回。
+///
+/// # 参数
+///
+/// * `path` - 表示要读取的文件路径的 `Path` 引用。
+///
+/// # 返回值
+///
+/// * `Result<String, std::io::Error>` - 如果成功，返回文件内容作为 `String`，如果发生错误，则返回 `std::io::Error`。
+///
+/// # 错误
+///
+/// 如果文件不存在、用户没有权限读取文件或发生其他 I/O 错误，此函数将返回错误。
 fn read_temperature(path: PathBuf) -> Result<Temperature> {
 	let contents = read_file(&path)?;
 	match contents.trim_end().parse::<f64>() {
@@ -35,6 +49,11 @@ fn hwmon_sensor(input: PathBuf) -> Result<TemperatureSensor> {
 	// that it is not a root directory and it points to a file.
 	// Otherwise it is an implementation bug.
 	let root = input.parent().unwrap_or_else(|| unreachable!());
+	let sensor_id: Option<String> = root
+		.file_name()
+		.and_then(|s| s.to_str().map(|s| s.to_string()));
+
+	// 从给定的 `input` 文件名中提取前缀，通过移除后缀 "input"。
 	let prefix = match input.file_name() {
 		Some(name) => {
 			let offset = name.len() - b"input".len();
@@ -71,6 +90,13 @@ fn hwmon_sensor(input: PathBuf) -> Result<TemperatureSensor> {
 		None
 	};
 
+	let min_path = root.join(file_name(prefix, b"min"));
+	let min = if min_path.exists() {
+		Some(read_temperature(min_path)?)
+	} else {
+		None
+	};
+
 	let current = read_temperature(input)?;
 
 	Ok(TemperatureSensor {
@@ -79,9 +105,12 @@ fn hwmon_sensor(input: PathBuf) -> Result<TemperatureSensor> {
 		current,
 		max,
 		crit,
+		min,
+		sensor_id,
 	})
 }
 
+/// 硬件传感器（Hardware Monitoring）
 // https://github.com/shirou/gopsutil/blob/2cbc9195c892b304060269ef280375236d2fcac9/host/host_linux.go#L624
 fn hwmon() -> Vec<Result<TemperatureSensor>> {
 	let mut glob_results = glob("/sys/class/hwmon/hwmon*/temp*_input");
@@ -102,6 +131,17 @@ fn hwmon() -> Vec<Result<TemperatureSensor>> {
 		.collect()
 }
 
+/// ACPI 热管理（Thermal Zones）
+///
+/// acpitz：表示 ACPI 热管理区域（通常是主板传感器）
+///
+/// cpu-thermal：CPU 的温度
+///
+/// soc-thermal：表示 SoC（System on Chip）整体温度（常见于 ARM 设备）
+///
+/// gpu-thermal：表示 GPU 温度
+///
+/// x86_pkg_temp：表示 Intel CPU 封装温度（通常适用于 Intel CPU）
 // https://www.kernel.org/doc/Documentation/thermal/sysfs-api.txt
 fn thermal_zone() -> Vec<Result<TemperatureSensor>> {
 	glob("/sys/class/thermal/thermal_zone*")
@@ -150,6 +190,8 @@ fn thermal_zone() -> Vec<Result<TemperatureSensor>> {
 				current,
 				max,
 				crit,
+				min: None,
+				sensor_id: None,
 			})
 		})
 		.collect()
